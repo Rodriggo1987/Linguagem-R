11c3c2284575683ed0d0cb822cd4468f164b87e2
Rodriggo Nascimento <79999840+Rodriggo1987@users.noreply.github.com>
2024-02-25 15:11:07 -0300

    Add files via upload

---
+++ Sobre_dplyr.R
@@ -0,0 +1,18 @@
+# temos algumas opções aqui - podemos carregar todo o tidyverse ou apenas carregar o
+# pacotes tidyverse que estamos interessados em usar. Vou carregar o tidyverse, 
+# mas alternativamente você
+# poderia executar o seguinte:
+
+
+library(readr)
+library(dplyr)
+
+# load the tidyverse
+library(tidyverse)
+
+penguins <- read_csv('../input/palmer-archipelago-antarctica-penguin-data/penguins_size.csv')
+
+
+
+
+
